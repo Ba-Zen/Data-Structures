72d8521a13ed0abb8b97541c785053633b8b127a
Ba-Zen <bazenatlas@gmail.com>
2019-09-16 17:02:34 -0400

    queue and stack complete and tested

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,18 +1,22 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('doubly_linked_list')
+
 
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = []
 
-  def enqueue(self, value):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, value):
+        self.storage.insert(0, value)
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if not self.storage:
+            return
+        else:
+            return self.storage.pop()
+
+    def len(self):
+        return len(self.storage)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,31 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('doubly_linked_list')
+
 
 class Stack:
-  def __init__(self):
-    self.size = 0
-    # Why is our DLL a good choice to store our elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = []
 
-  def push(self, value):
-    pass
-  
-  def pop(self):
-    pass
+    def push(self, value):
+        self.storage.insert(0, value)
 
-  def len(self):
-    pass
+    def pop(self):
+        if not self.storage:
+            return None
+        else:
+            return self.storage.pop(0)
+
+    def len(self):
+        return len(self.storage)
+
+
+s = Stack()
+s.push(1)
+s.push(2)
+s.push(3)
+s.push(4)
+print(s.storage)
+print(s.pop())
